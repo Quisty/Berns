0363d1e948ecdae76ef4c0b54b981bcbbdae9b16
Quisty <magnusquist2@yahoo.dk>
2016-11-18 09:42:14 +0100

    Add files via upload

---
+++ lotr/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="js/materialize/materialize.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap/bootstrap.js" type="text/javascript"></script>
+	<link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/materialize/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>New Zealand - Lord of the Rings</title>
+</head>
+<body>
+
+	<!-- Navbar (Fixed) -->
+	<div class="navbar-fixed">
+		<nav class="grey darken-3">
+			<div class="nav-wrapper">
+				<a href="index.php" class="brand-logo">New Zealand</a>
+				<ul class="right hide-on-med-and-down">
+					<li><a data-scroll href="#locations">Filming Locations</a></li>
+					<li><a data-scroll href="#timespan">Timespan</a></li>
+					<li><a data-scroll href="#?">??????</a></li>
+					<li><a data-scroll href="#?">??????</a></li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+
+	<!-- Content -->
+	<div class="parallax-container" style="margin-top: -75px;">
+		<div class="parallax"><img src="img/landscape1.jpg"></div>
+	</div>
+	<div class="section grey lighten-2">
+		<div class="row container">
+			<h2 id="locations" class="header">Filming Locations</h2>
+			<div class="row">
+				<h5>Matamata</h5>
+				<img class="materialboxed right" width="150px" height="110px" src="img/locations/matamata.jpg">
+			</div>
+			<div class="row">
+				<h5>Wellington</h5>
+				<img class="materialboxed right" width="150px" height="110px" src="img/locations/wellington.jpg">
+			</div>
+			<div class="row">
+				<h5>Canterbury</h5>
+				<img class="materialboxed right" width="150px" height="110px" src="img/locations/canterbury.jpg">
+			</div>
+			<div class="row">
+				<h5>Mackenzie Country</h5>
+				<img class="materialboxed right" width="150px" height="110px" src="img/locations/mackenzie country.jpg">
+			</div>
+			<div class="row">
+				<h5>Southern Lakes</h5>
+				<img class="materialboxed right" width="150px" height="110px" src="img/locations/southern lakes.jpg">
+			</div>
+			<div class="row">
+				<h5>Nelson</h5>
+				<img class="materialboxed right" width="150px" height="110px" src="img/locations/nelson.jpg">
+			</div>
+		</div>
+	</div>
+	<div class="parallax-container">
+		<div class="parallax"><img src="img/landscape2.jpg"></div>
+	</div>
+</body>
+<script src="js/animations.js"></script>
+<script src="js/parallax.js"></script>
+<script src="js/smooth-scroll.js"></script>
+<script> smoothScroll.init(); </script>
+</html>
---
+++ lotr/js/animations.js
@@ -0,0 +1,4 @@
+$('.brand-logo').hover(
+       function(){ $(this).addClass('animated infinite tada') },
+       function(){ $(this).removeClass('animated infinite tada') }
+);
