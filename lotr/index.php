7935384b5abff7ef91fdfce32d32af2b6b1f3fd8
Quisty <magnusquist2@yahoo.dk>
2016-11-22 21:53:43 +0100

    Add files via upload

--- lotr/index.php
+++ lotr/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="js/materialize/materialize.min.js" type="text/javascript"></script>
 	<script src="js/bootstrap/bootstrap.js" type="text/javascript"></script>
@@ -21,8 +22,7 @@
 				<ul class="right hide-on-med-and-down">
 					<li><a data-scroll href="#locations">Filming Locations</a></li>
 					<li><a data-scroll href="#timespan">Timespan</a></li>
-					<li><a data-scroll href="#?">??????</a></li>
-					<li><a data-scroll href="#?">??????</a></li>
+					<li><a data-scroll href="#tourists">Tourists</a></li>
 				</ul>
 			</div>
 		</nav>
@@ -32,41 +32,121 @@
 	<div class="parallax-container" style="margin-top: -75px;">
 		<div class="parallax"><img src="img/landscape1.jpg"></div>
 	</div>
-	<div class="section grey lighten-2">
-		<div class="row container">
-			<h2 id="locations" class="header">Filming Locations</h2>
-			<div class="row">
-				<h5>Matamata</h5>
-				<img class="materialboxed right" width="150px" height="110px" src="img/locations/matamata.jpg">
-			</div>
-			<div class="row">
-				<h5>Wellington</h5>
-				<img class="materialboxed right" width="150px" height="110px" src="img/locations/wellington.jpg">
-			</div>
-			<div class="row">
-				<h5>Canterbury</h5>
-				<img class="materialboxed right" width="150px" height="110px" src="img/locations/canterbury.jpg">
-			</div>
-			<div class="row">
-				<h5>Mackenzie Country</h5>
-				<img class="materialboxed right" width="150px" height="110px" src="img/locations/mackenzie country.jpg">
-			</div>
-			<div class="row">
-				<h5>Southern Lakes</h5>
-				<img class="materialboxed right" width="150px" height="110px" src="img/locations/southern lakes.jpg">
-			</div>
-			<div class="row">
-				<h5>Nelson</h5>
-				<img class="materialboxed right" width="150px" height="110px" src="img/locations/nelson.jpg">
-			</div>
+
+
+
+	<div class="grey darken-3" id="locations">
+		<div class="slider">
+			<ul class="slides">
+				<li>
+					<img src="img/locations/filming.jpg"> <!-- random image -->
+					<div class="caption center-align">
+						<h3>Filming Locations</h3>
+						<h5 class="light grey-text text-lighten-3">The Lord of The Rings was filmed in the beautiful landscapes in New Zealand</h5>
+					</div>
+				</li>
+				<li>
+					<img src="img/locations/matamata.jpg"> <!-- random image -->
+					<div class="caption center-align">
+						<h3>Matamata</h3>
+						<h5 class="light grey-text text-lighten-3">The home place of Hobbits</h5>
+					</div>
+				</li>
+				<li>
+					<img src="img/locations/nelson.jpg"> <!-- random image -->
+					<div class="caption left-align">
+						<h3>Nelson</h3>
+						<h5 class="light grey-text text-lighten-3">Creation of the rings</h5>
+					</div>
+				</li>
+				<li>
+					<img src="img/locations/canterbury.jpg"> <!-- random image -->
+					<div class="caption center-align">
+						<h3>Canterbury</h3>
+						<h5 class="light grey-text text-lighten-3">Edoras, the city of Rohan</h5>
+					</div>
+				</li>
+				<li>
+					<img src="img/locations/mackenzie_country.jpg"> <!-- random image -->
+					<div class="caption center-align">
+						<h3>Mackenzie Country</h3>
+						<h5 class="light grey-text text-lighten-3">The Great Battle</h5>
+					</div>
+				</li>
+				<li>
+					<img src="img/locations/southern_lakes.jpg"> <!-- random image -->
+					<div class="caption right-align">
+						<h3>Southern Lakes</h3>
+						<h5 class="light grey-text text-lighten-3">The Two Towers</h5>
+					</div>
+				</li>
+				<li>
+					<img src="img/locations/wellington.jpg"> <!-- random image -->
+					<div class="caption center-align">
+						<h3>Wellington</h3>
+						<h5 class="light grey-text text-lighten-3">Hiding from the Black Knights</h5>
+					</div>
+				</li>
+			</ul>
+			<div class="grey darken-3" style="height: 50px;"></div>
 		</div>
 	</div>
+
+
+
 	<div class="parallax-container">
 		<div class="parallax"><img src="img/landscape2.jpg"></div>
 	</div>
+
+
+
+	<div class="grey darken-3" id="timespan">
+		<div class="slider">
+			<ul class="slides">
+				<li>
+					<img class="fullWidth" src="img/timespan/timeline.jpg">
+					<div class="caption center-align">
+						<h3>Timespan</h3>
+						<h5 class="light grey-text text-lighten-3">Movie Timespan -- Actual Timespan</h5>
+					</div>
+				</li>
+			</ul>
+			<div class="grey darken-3" style="height: 50px;"></div>
+		</div>
+	</div>
+
+
+
+	<div class="parallax-container">
+		<div class="parallax"><img src="img/landscape3.jpg"></div>
+	</div>
+
+
+
+	<div class="grey darken-3" id="tourists">
+		<div class="slider">
+			<ul class="slides">
+				<li>
+					<img class="fullWidth" src="img/tourists/matamata.jpg">
+					<div class="caption center-align">
+						<h3>For The Touritsts</h3>
+						<h5 class="light grey-text text-lighten-3">The opertunity to see the places in person</h5>
+					</div>
+				</li>
+			</ul>
+			<div class="grey darken-3" style="height: 50px;"></div>
+		</div>
+	</div>
+
+
+	
+	<div class="parallax-container">
+		<div class="parallax"><img src="img/landscape4.jpg"></div>
+	</div>	
 </body>
 <script src="js/animations.js"></script>
 <script src="js/parallax.js"></script>
 <script src="js/smooth-scroll.js"></script>
 <script> smoothScroll.init(); </script>
+<script src="js/slider.js"></script>
 </html>
---
+++ lotr/js/slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.slider').slider({
+		full_width: true,
+		indicators: true,
+		height: 600,
+		interval: 1000000
+	});
+});
